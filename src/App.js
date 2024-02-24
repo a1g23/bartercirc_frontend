8c55d47a80fbb0b8964053042886b90ab764bc7c
Alex Greenberg <acgreenberg2@gmail.com>
2024-02-24 15:49:28 -0500

    first pass at styling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header"
 
 
+
 function App() {
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 import router from "./router"
 import { RouterProvider } from 'react-router-dom'
+import "./style.scss"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
