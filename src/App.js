4676dd39015ef52e6aeb67594e76643c6dfbfe1e
rohansinha01 <rohansinha01@gmail.com>
2024-02-24 12:24:13 -0500

    App.js updated

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 // import './App.css';
 import { Outlet } from "react-router-dom"
 import Footer from "./components/Footer";
+import Header from "./components/Header"
 
 
 function App() {
   return (
     <div className="App">
+        <Header />
         <Outlet />
+        <Footer />
     </div>
   );
 }
