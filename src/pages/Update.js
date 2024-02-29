004816887e8b14a43d0880525a6b88ce465f7e59
Alex Greenberg <acgreenberg2@gmail.com>
2024-02-28 20:39:22 -0500

    updated update page

--- src/pages/Update.js
+++ src/pages/Update.js
@@ -4,18 +4,42 @@ const Update = () => {
     const singleItem = useLoaderData()
     return (
         <div>
-            <h1>{singleItem.itemName}</h1>
-            <h1>Update Item</h1>
-            <Form action={`/updating/${singleItem._id}`} method='post' className="form-class">
-                <fieldset>
-                  <legend></legend> 
-                 <label>Seller</label><input  className="input-class" type='input' name='seller' defaultValue={singleItem.seller} />
-                <label>Name</label><input  className="input-class" type='input' name='itemName' defaultValue={singleItem.itemName} />
-                <label>Description</label><input  className="input-class" type='input' name='description' defaultValue={singleItem.description} />
-                <label>Quantity</label><input  className="input-class" type='input' name='quantity' defaultValue={singleItem.quantity} />
-                <label>Price</label><input  className="input-class" type='input' name='price' defaultValue={singleItem.price} />
-                <input  className="input-class" type='submit' value={`Update ${singleItem.itemName}`}/>
-            </fieldset>
+            <h1 className="update-title">{singleItem.itemName}</h1>
+            
+            <Form action={`/updating/${singleItem._id}`} method='post' className="update-form-class">
+
+                
+                    <div className="update-grid-seller">
+                        <label>Seller:</label>
+                        <input className="update-input-class" type='input' name='seller' defaultValue={singleItem.seller} />
+                    </div>  
+                    
+                    <div className="update-grid-name">
+                        <label>Name:</label>
+                        <input  className="update-input-class" type='input' name='itemName' defaultValue={singleItem.itemName} />
+                    </div> 
+                
+                
+
+                <div className="update-grid-description">
+                    <label>Description:</label>
+                    <input  className="update-input-description" type='text' name='description' defaultValue={singleItem.description} />
+                </div> 
+
+                
+                    <div className="update-grid-quantity">
+                        <label>Quantity:</label>
+                        <input  className="update-input-quantity" type='input' name='quantity' defaultValue={singleItem.quantity} />
+                    </div> 
+
+                    <div className="update-grid-price">
+                        <label>$$:</label>
+                        <input  className="update-input-price" type='input' name='price' defaultValue={singleItem.price} />
+                    </div> 
+               
+
+                <input  className="update-input-button" type='submit' value={`Update ${singleItem.itemName}`}/>
+                
             </Form>
             
         </div>
