197288bddf28c6bedc911d681b4a5e42245631db
rohansinha01 <rohansinha01@gmail.com>
2024-02-28 19:27:23 -0500

    typos

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -6,10 +6,10 @@ const Show = () => {
         <div className="show-container">
             <h1 className="show-item show-item-title">{singleItem.itemName}</h1>
             <h3 className="show-item show-item-seller">Seller: {singleItem.seller}</h3>
-            <h3 className="show-item show-item-name">Item Name: {singleItem.itemName}</h3>
-            <h3 className="show-item show-item-description">Item Description: {singleItem.description}</h3>
-            <h3 className="show-item show-item-quantity">Item Quantity: {singleItem.quantity}</h3>
-            <h3 className="show-item show-item-price">Item Price: {singleItem.price}</h3>
+            <h3 className="show-item show-item-name">Name: {singleItem.itemName}</h3>
+            <h3 className="show-item show-item-description">Description: {singleItem.description}</h3>
+            <h3 className="show-item show-item-quantity">Quantity: {singleItem.quantity}</h3>
+            <h3 className="show-item show-item-price">Price: ${singleItem.price}</h3>
             <div className="show-button-container">
                 <Form action={`/update/${singleItem._id}`}>
                     <input type='submit' value={`Update ${singleItem.itemName}`} className="show-update-button"/>
--- src/pages/Update.js
+++ src/pages/Update.js
@@ -7,12 +7,15 @@ const Update = () => {
             <h1>{singleItem.itemName}</h1>
             <h1>Update Item</h1>
             <Form action={`/updating/${singleItem._id}`} method='post' className="form-class">
-                <input  className="input-class" type='input' name='seller' defaultValue={singleItem.seller} />
-                <input  className="input-class" type='input' name='itemName' defaultValue={singleItem.itemName} />
-                <input  className="input-class" type='input' name='description' defaultValue={singleItem.description} />
-                <input  className="input-class" type='input' name='quantity' defaultValue={singleItem.quantity} />
-                <input  className="input-class" type='input' name='price' defaultValue={singleItem.price} />
-                <input  className="input-class" type='submit' value={`update ${singleItem.itemName}`}/>
+                <fieldset>
+                  <legend></legend> 
+                 <label>Seller</label><input  className="input-class" type='input' name='seller' defaultValue={singleItem.seller} />
+                <label>Name</label><input  className="input-class" type='input' name='itemName' defaultValue={singleItem.itemName} />
+                <label>Description</label><input  className="input-class" type='input' name='description' defaultValue={singleItem.description} />
+                <label>Quantity</label><input  className="input-class" type='input' name='quantity' defaultValue={singleItem.quantity} />
+                <label>Price</label><input  className="input-class" type='input' name='price' defaultValue={singleItem.price} />
+                <input  className="input-class" type='submit' value={`Update ${singleItem.itemName}`}/>
+            </fieldset>
             </Form>
             
         </div>
